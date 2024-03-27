6ccef9601b2b6a2ad6461f9a9056aae11d798e37
Gustavo Bacagine <gustavo.bacagine@protonmail.com>
2024-03-26 22:10:48 -0300

    add more make scripts and change the identation of the file player.h

--- include/player.h
+++ include/player.h
@@ -9,42 +9,42 @@
  */
 
 #ifndef _PLAYER_H_
-#define _PLAYER_H_
+  #define _PLAYER_H_
 
-#include <consts.h>
-#include <util.h>
+  #include <consts.h>
+  #include <util.h>
 
-#define PLAYERS_FILE "players.dat"
-#define SCORE       100 // pontucao para cada fase
+  #define PLAYERS_FILE "players.dat"
+  #define SCORE        100 // pontucao para cada fase
 
-typedef struct STRUCT_CATTIE_PLAYER {
-  char szName[256];
-  int iScore;
-} STRUCT_CATTIE_PLAYER, *PSTRUCT_CATTIE_PLAYER;
+  typedef struct STRUCT_CATTIE_PLAYER {
+    char szName[256];
+    int iScore;
+  } STRUCT_CATTIE_PLAYER, *PSTRUCT_CATTIE_PLAYER;
 
-extern STRUCT_CATTIE_PLAYER gstCattiePlayer;
-extern char gszPlayersFilePath[256];
+  extern STRUCT_CATTIE_PLAYER gstCattiePlayer;
+  extern char gszPlayersFilePath[256];
 
-/**
- * Set the path of the file players.dat
- */
-void vSetPlayerFilePath( const char *kpszPath );
+  /**
+   * Set the path of the file players.dat
+   */
+  void vSetPlayerFilePath( const char *kpszPath );
 
-void vUpdatePlayerScore( void );
+  void vUpdatePlayerScore( void );
 
-int iGetPlayerScore( void );
+  int iGetPlayerScore( void );
 
-void vSetPlayerName( const char *kpszName );
+  void vSetPlayerName( const char *kpszName );
 
-/**
- * Register a score of a player in players.dat
- */
-int iRegisterPlayer( void );
+  /**
+   * Register a score of a player in players.dat
+   */
+  int iRegisterPlayer( void );
 
-/**
- * Remove a player from players.dat
- */
-int iRemovePlayer( void );
+  /**
+   * Remove a player from players.dat
+   */
+  int iRemovePlayer( void );
 
 #endif /* _PLAYER_H_ */
 
